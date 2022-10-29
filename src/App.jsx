aa3145d59c4f4ab16776ec7020cf7599fcf1b3d4
CheIby <ootcazy007@gmail.com>
2022-10-29 18:13:33 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { useState,useEffect } from 'react';
+
+function App() {
+  const [text,setText] = useState([])
+
+  const fetchData=async()=>{
+    await axios.get('http://localhost:7000/getall')
+    .then(res=>{
+        setText(res.data)
+    }).catch(err=>{
+        alert(err)
+    })
+}
+
+  useEffect(()=>{
+    fetchData()
+    
+  },[])
+  console.log()
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {text.map((data,index)=>
+          <h1 key={index}>{data.msg}</h1>
+        )}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
